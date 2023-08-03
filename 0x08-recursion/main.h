a6a6f4d785a6767e3b3d67c574c1cd41e991fe41
OVOcurry <victorikedi21@gmail.com>
2023-08-02 20:24:23 -0700

    string Palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,8 @@ int is_prime_number(int n);
 int _putchar(char c);
 int actual_sqrt_recursion(int n, int i);
 int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+int is_palindrome(char *s);
 
 #endif
