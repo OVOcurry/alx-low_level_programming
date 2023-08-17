0a37ef00834c213d4051756cc771bd7cca7a4a7f
OVOcurry <victorikedi21@gmail.com>
2023-08-16 21:29:32 -0700

    i create opportunities

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
-#include “function_pointers.h”
+#include "function_pointers.h"
 /**
-* array_iterator - prints each array elem on a newl
+ * array_iterator - prints each array elem on a newl
  * @array: array
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array == NULL || action == NULL)
-		Return;
+		return;
 
 	for (i = 0; i < size; i++)
 	{
