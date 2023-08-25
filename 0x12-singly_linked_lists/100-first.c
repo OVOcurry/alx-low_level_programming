b6ae6aaec1413c95d9ebf43e014582aef8ce0f40
OVOcurry <victorikedi21@gmail.com>
2023-08-24 17:59:49 -0700

    linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "list.h"
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - Before the main rints a sentence
+ * function is executed
+ */
+void first(void)
+{
+ printf("You're beat! and yet, you must allow,\n");
+ printf("I bore my house upon my back!\n");
+}
